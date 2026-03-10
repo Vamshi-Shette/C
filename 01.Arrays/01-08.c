334305aa781c73411cc8a0e8ae022bed21b6aa7d
Vamshi-Shette <vamshishette@gmail.com>
2026-03-10 12:25:19 +0530

    added files

--- 01.Arrays/01-08.c
+++ 01.Arrays/01-08.c
@@ -17,6 +17,14 @@ void print(int *arr,int num)
         printf("\n");
         return;
 }
+/*
+Question: Sum of all elements in array
+Question description: Write a C program to find the sum of all array elements entered by the user.
+Sample data:
+Input: 1 2 3 4 5
+Expected output:
+Sum = 15
+*/
 void sum_of_elements(int *arr,int num)
 {
         int sum=0;
@@ -25,6 +33,15 @@ void sum_of_elements(int *arr,int num)
         printf("sum=%d\n",sum);
         return;
 }
+/*
+Question: Find maximum and minimum element
+Question description: Write a C program to find the largest and smallest element in the array.
+Sample data:
+Input: 8 3 15 6 2
+Expected output:
+Maximum = 15
+Minimum = 2
+ */       
 void min_max(int *arr,int num)
 {
         int min,max=arr[0];
@@ -39,6 +56,16 @@ void min_max(int *arr,int num)
         printf("maximum=%d\nminimum=%d\n",max,min);
         return;
 }
+
+/*
+Question: Count even and odd elements
+Question description: Write a C program to count how many even and odd numbers are in the array.
+Sample data:
+Input: 4 7 9 10 22
+Expected output:
+Even: 3
+Odd: 2
+*/
 void count_even_odd(int *arr,int num)
 {
         int even=0,odd=0;
@@ -52,6 +79,15 @@ void count_even_odd(int *arr,int num)
         printf("even=%d\nodd=%d\n",even,odd);
         return;
 }
+
+/*
+Question: Copy array elements
+Question description: Write a C program to copy all elements from one array to another.
+Sample data:
+Input: 1 2 3 4 5
+Expected output:
+Copied array: 1 2 3 4 5
+*/
 void copy_array(int *arr,int num)
 {
         static int arr2[20];
@@ -63,6 +99,50 @@ void copy_array(int *arr,int num)
         }
         return;
 }
+
+/*
+Question: Count negative elements
+
+Question description:
+Write a C program to count total number of negative elements in an array.
+
+Sample data:
+Input: -1 3 -4 5 0 -7
+
+Expected output:
+Total negative elements: 3
+*/
+
+void count_neg()
+{
+    int n, i, count = 0;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] < 0)
+        {
+            count++;
+        }
+    }
+    printf("Total negative elements: %d\n", count);
+        return;
+}
+/*
+Question: Find for an element
+Question description: Write a C program to search for a number in an array and print its index.
+Sample data:
+Array: 10 20 30 40 50
+Find: 30
+Expected output:
+Element found at index: 2
+*/
 void find_element(int *arr,int num)
 {
         int find,count=0;
@@ -78,6 +158,15 @@ void find_element(int *arr,int num)
                 printf("Not found\n");
         return;
 }
+
+/*
+Question: Reverse an array
+Question description: Write a C program to reverse an array in-place.
+Sample data:
+Input: 10 20 30 40 50
+Expected output:
+Reversed array: 50 40 30 20 10
+*/
 
 void rev_array(int *arr,int num)
 {
@@ -102,6 +191,7 @@ int main()
         printf("Enter the elements:");
         for(int i=0;i<num;i++)
                 scanf("%d",arr+i);
+        count_neg();
         print(arr,num);
         sum_of_elements(arr);
         min_max(arr,num);
@@ -111,4 +201,5 @@ int main()
         rev_array(arr,num);
         return 0;
 }
+
      
